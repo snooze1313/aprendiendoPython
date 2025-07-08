bc6db524658ce11827c99eec487b87294ba7b5b5
Omar Alexis Gerardo Diaz Nuñez <63167741+snooze1313@users.noreply.github.com>
2025-07-08 13:13:56 -0700

    create importModules
    incomplete (part 1)

---
+++ importModules.py
@@ -0,0 +1,9 @@
+#para importar un modulo se usa la palabra clave import
+
+import time #modulo de tiempo
+
+#uso de la funcion asctime del modulo time
+fecha = time.asctime() #funcion del modulo time
+print(fecha)
+
+#
